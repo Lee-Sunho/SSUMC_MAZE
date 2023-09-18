30fed9a826a0d1c7c6231b8014505852bd620788
Lee-Sunho <sunho9889@naver.com>
2023-09-19 01:37:07 +0900

    feat: 랭킹 화면 구현

--- app/api/rank/route.ts
+++ app/api/rank/route.ts
@@ -1,13 +1,20 @@
 import { connectDB } from "../../../util/database";
 import { NextRequest, NextResponse } from "next/server";
 
+interface IRank {
+  userName: string;
+  record: number;
+}
 export async function GET(req: Request) {
   const db = (await connectDB).db("rank");
 
   try {
-    const rankings = await db.collection("rank").find().toArray();
-    console.log(rankings);
-    return NextResponse.json(rankings);
+    const result = await db.collection("rank").find().limit(10).toArray();
+    console.log(result);
+    const ranking = result.sort((a: IRank, b: IRank) => {
+      return a.record - b.record;
+    });
+    return NextResponse.json(ranking);
   } catch (error) {
     console.log(error);
   }
--- app/rank/page.tsx
+++ app/rank/page.tsx
@@ -1,18 +1,49 @@
 "use client";
 
 import axios from "axios";
+import "./style.css";
+import { useEffect, useState } from "react";
+import { ObjectId } from "mongodb";
+import { useRouter } from "next/navigation";
+
+interface IRecord {
+  rank: number;
+  userName: string;
+  record: number;
+}
+
+interface IRank {
+  id: ObjectId;
+  userName: string;
+  record: number;
+}
+
+const Record = ({ rank, userName, record }: IRecord) => {
+  return (
+    <div className="record_wrapper">
+      <div className="record_rank">{rank}</div>
+      <div className="record_name">{userName}</div>
+      <div className="record_record">{`${record} s`}</div>
+    </div>
+  );
+};
 
 const Rank = () => {
+  document.querySelector(".Rank");
+  const [rank, setRank] = useState<IRank[]>([]);
   const apiUrl = "/api/rank";
+  const router = useRouter();
 
-  axios
-    .get(apiUrl)
-    .then((response) => {
-      console.log("API 응답 데이터:", response.data);
-    })
-    .catch((error) => {
-      console.log("API 요청 실패:", error);
-    });
+  useEffect(() => {
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setRank(response.data);
+      })
+      .catch((error) => {
+        console.log("API 요청 실패:", error);
+      });
+  }, []);
 
   const userName = "mark";
   const record = 45;
@@ -30,9 +61,35 @@ const Rank = () => {
         console.log("API 요청 실패:", error);
       });
   };
+
+  const exit = () => {
+    router.replace("/");
+  };
   return (
-    <div onClick={onClick}>
-      <button>랭킹 등록</button>
+    <div className="Rank">
+      <img className="img_char_rank" src="/assets/img_char_ranking.svg" />
+      <button onClick={exit} className="btn_close" />
+      <div className="rank_background">
+        <div className="rank_container">
+          <div className="rank_header">
+            <span>RANK</span>
+            <span>NAME</span>
+            <span>TIME</span>
+          </div>
+          <div className="rank_contents">
+            {rank.map((item, index) => {
+              return (
+                <Record
+                  key={index}
+                  rank={index + 1}
+                  userName={item.userName}
+                  record={item.record}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
